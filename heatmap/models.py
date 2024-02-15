c50410cb56b26281275691d5f0f31c10c6940ee1
suurgatio <serajsaboungi@ufl.edu>
2024-02-14 23:07:06 -0500

    created a model for the 'PlayerMovement' table

--- heatmap/models.py
+++ heatmap/models.py
@@ -1,4 +1,12 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
-# use this file to do the math (cartesian coordinates)
+
+# creating PlayerMovements data table
+class PlayerMovement(models.Model):
+    movement_id = models.BigAutoField(primary_key=True)  # MovementID as a primary key
+    session_date = models.DateField()  # SessionDate
+    timestamp = models.TimeField()  # Timestamp
+    latitude = models.FloatField()  # Latitude
+    longitude = models.FloatField()  # Longitude
+    heart_rate = models.IntegerField(null=True, blank=True)  # Heart Rate, allowing null values
